02e0f664c83cbfa042f5d2ce07f819524bccd1c4
marinakolova <marina.kolova.359@gmail.com>
2021-12-02 20:28:54 +0200

    added solutions for days 1 and 2

---
+++ AdventOfCode2021/Day01/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day01
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task01();
+            Task02();
+        }
+
+        static void Task02()
+        {
+            var numbers = new List<int>();
+            var largerCount = 0;
+            
+            while (true)
+            {
+                var input = Console.ReadLine();
+            
+                if (input == "end")
+                {
+                    break;
+                }
+            
+                numbers.Add(int.Parse(input));
+            }
+            
+            var firstWindow = 0;
+            var secondWindow = 0;
+            var thirdWindow = 0;
+            
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (i == 0)
+                {
+                    firstWindow += numbers[i];
+                } 
+                else if (i == 1)
+                {
+                    firstWindow += numbers[i];
+                    secondWindow += numbers[i];                    
+                }
+                else if (i == 2)
+                {
+                    firstWindow += numbers[i];
+                    secondWindow += numbers[i];
+                    thirdWindow += numbers[i];
+                }
+                else
+                {                    
+                    secondWindow += numbers[i];
+            
+                    if (secondWindow > firstWindow)
+                    {
+                        largerCount++;
+                    }
+            
+                    firstWindow = secondWindow;
+                    secondWindow = thirdWindow + numbers[i];
+                    thirdWindow = numbers[i];
+                }
+            }
+            
+            Console.WriteLine("Larger Count: " + largerCount);
+        }
+
+        static void Task01()
+        {
+            var largerCount = 0;
+            var previous = int.Parse(Console.ReadLine());
+            
+            while (true)
+            {
+                var input = Console.ReadLine();
+            
+                if (input == "end")
+                {
+                    break;
+                }
+            
+                var current = int.Parse(input);
+            
+                if (current > previous)
+                {
+                    largerCount++;
+                }
+            
+                previous = current;
+            }
+            
+            Console.WriteLine("Larger Count: " + largerCount);
+        }
+    }
+}
---
+++ AdventOfCode2021/Day02/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace Day02
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task01();
+            Task02();
+        }
+
+        static void Task02()
+        {
+            var horizontalPosition = 0;
+            var depth = 0;
+            var aim = 0;
+
+            while (true)
+            {
+                var input = Console.ReadLine();
+
+                if (input == "end")
+                {
+                    break;
+                }
+
+                var inputProps = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                var keyWord = inputProps[0];
+                var number = int.Parse(inputProps[1]);
+
+                switch (keyWord)
+                {
+                    case "forward":
+                        horizontalPosition += number;
+                        depth += (aim * number);
+                        break;
+
+                    case "down":
+                        aim += number;
+                        break;
+
+                    case "up":
+                        aim -= number;
+                        break;
+                }
+            }
+
+            Console.WriteLine($"Result: {horizontalPosition * depth}");
+        }
+
+        static void Task01()
+        {
+            var horizontalPosition = 0;
+            var depth = 0;
+            
+            while (true)
+            {
+                var input = Console.ReadLine();
+            
+                if (input == "end")
+                {
+                    break;
+                }
+            
+                var inputProps = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                var keyWord = inputProps[0];
+                var number = int.Parse(inputProps[1]);
+            
+                switch (keyWord)
+                {
+                    case "forward": 
+                        horizontalPosition += number;
+                        break;
+            
+                    case "down": 
+                        depth += number;
+                        break;
+            
+                    case "up": 
+                        depth -= number;
+                        break;
+                }
+            }
+            
+            Console.WriteLine($"Result: {horizontalPosition * depth}");
+        }
+    }
+}
