d61d9646e7e9bbad46d5fde8c1a6d3b04d100e3c
marinakolova <marina.kolova.359@gmail.com>
2021-12-07 20:08:01 +0200

    added day 7 solution

---
+++ AdventOfCode2021/Day07/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Linq;
+
+namespace Day07
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Task01();
+            Task02();
+            
+        }
+
+        private static void Task02()
+        {
+            var input = Console.ReadLine()
+                .Split(",", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var minPosition = input.Min();
+            var maxPosition = input.Max();
+            var minFuel = int.MaxValue;
+
+            for (int position = minPosition; position <= maxPosition; position++)
+            {
+                var fuel = 0;
+
+                for (int i = 0; i < input.Length; i++)
+                {
+                    var distance = Math.Abs(input[i] - position);
+                    fuel += (1 + distance) * distance / 2;
+                }
+
+                if (fuel < minFuel)
+                {
+                    minFuel = fuel;
+                }
+            }
+
+            Console.WriteLine("Min Fuel: " + minFuel);
+        }
+
+        public static void Task01()
+        {
+            var input = Console.ReadLine()
+                .Split(",", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var minPosition = input.Min();
+            var maxPosition = input.Max();
+            var minFuel = int.MaxValue;
+
+            for (int position = minPosition; position <= maxPosition; position++)
+            {
+                var fuel = 0;
+
+                for (int i = 0; i < input.Length; i++)
+                {
+                    fuel += Math.Abs(input[i] - position);
+                }
+
+                if (fuel < minFuel)
+                {
+                    minFuel = fuel;
+                }
+            }
+
+            Console.WriteLine("Min Fuel: " + minFuel);
+        }
+    }
+}
