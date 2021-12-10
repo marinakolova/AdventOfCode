c3a85744968796b730cedbeb2bffe8afc0b6458c
marinakolova <marina.kolova.359@gmail.com>
2021-12-10 21:11:29 +0200

    added days 8, 9 and 10 solutions

---
+++ AdventOfCode2021/Day09/Program.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day09
+{
+    public class Program
+    {
+        private static List<HashSet<ValueTuple<int, int>>> basins = new List<HashSet<ValueTuple<int, int>>>();
+
+        public static void Main(string[] args)
+        {
+            //Task01();
+            Task02();
+        }
+
+        public static void Task02()
+        {
+            var matrix = new List<int[]>();
+
+            while (true)
+            {
+                var inputLine = Console.ReadLine();
+
+                if (inputLine == "end")
+                {
+                    break;
+                }
+
+                matrix.Add(new int[inputLine.Length]);
+                for (int i = 0; i < inputLine.Length; i++)
+                {
+                    matrix[matrix.Count - 1][i] = int.Parse(inputLine[i].ToString());
+                }
+            }            
+
+            for (int row = 0; row < matrix.Count; row++)
+            {
+                for (int col = 0; col < matrix[0].Length; col++)
+                {
+                    var element = matrix[row][col];
+
+                    var up = row != 0 ? matrix[row - 1][col] : int.MaxValue;
+                    var down = row != matrix.Count - 1 ? matrix[row + 1][col] : int.MaxValue;
+                    var left = col != 0 ? matrix[row][col - 1] : int.MaxValue;
+                    var right = col != matrix[0].Length - 1 ? matrix[row][col + 1] : int.MaxValue;
+
+                    if (element < up && element < down && element < left && element < right)
+                    {
+                        basins.Add(new HashSet<(int, int)>());
+                        var currentBasin = basins.Count - 1;
+
+                        var currentRow = row;
+                        var currentCol = col;
+
+                        //check up
+                        while (currentRow >= 0)
+                        {
+                            if (matrix[currentRow][currentCol] == 9)
+                            {
+                                break;
+                            }
+                            else
+                            {
+                                basins[currentBasin].Add((currentRow, currentCol));
+                                CheckBasin(matrix, currentBasin, currentRow, currentCol);
+                            }
+                            currentRow -= 1;
+                        }
+                        currentRow = row;
+
+                        //check down
+                        while (currentRow < matrix.Count)
+                        {
+                            if (matrix[currentRow][currentCol] == 9)
+                            {
+                                break;
+                            }
+                            else
+                            {
+                                basins[currentBasin].Add((currentRow, currentCol));
+                                CheckBasin(matrix, currentBasin, currentRow, currentCol);
+                            }
+                            currentRow += 1;
+                        }
+                        currentRow = row;
+
+                        //check left
+                        while (currentCol >= 0)
+                        {
+                            if (matrix[currentRow][currentCol] == 9)
+                            {
+                                break;
+                            }
+                            else
+                            {
+                                basins[currentBasin].Add((currentRow, currentCol));
+                                CheckBasin(matrix, currentBasin, currentRow, currentCol);
+                            }
+                            currentCol -= 1;
+                        }
+                        currentCol = col;
+
+                        //check right
+                        while (currentCol < matrix[0].Length)
+                        {
+                            if (matrix[currentRow][currentCol] == 9)
+                            {
+                                break;
+                            }
+                            else
+                            {
+                                basins[currentBasin].Add((currentRow, currentCol));
+                                CheckBasin(matrix, currentBasin, currentRow, currentCol);
+                            }
+                            currentCol += 1;
+                        }                 
+                    }
+                }
+            }
+
+            var sortedBasins = basins.OrderByDescending(x => x.Count).ToList();
+            var result = sortedBasins[0].Count * sortedBasins[1].Count * sortedBasins[2].Count;
+            Console.WriteLine("Result: " + result);
+        }
+
+        private static void CheckBasin(List<int[]> matrix, int currentBasin, int row, int col)
+        {
+            var currentRow = row;
+            var currentCol = col;
+
+            //check up
+            while (currentRow >= 0)
+            {
+                if (matrix[currentRow][currentCol] == 9)
+                {
+                    break;
+                }
+                else
+                {
+                    basins[currentBasin].Add((currentRow, currentCol));
+                    CheckBasinAgain(matrix, currentBasin, currentRow, currentCol);
+                }
+                currentRow -= 1;
+            }
+            currentRow = row;
+
+            //check down
+            while (currentRow < matrix.Count)
+            {
+                if (matrix[currentRow][currentCol] == 9)
+                {
+                    break;
+                }
+                else
+                {
+                    basins[currentBasin].Add((currentRow, currentCol));
+                    CheckBasinAgain(matrix, currentBasin, currentRow, currentCol);
+                }
+                currentRow += 1;
+            }
+            currentRow = row;
+
+            //check left
+            while (currentCol >= 0)
+            {
+                if (matrix[currentRow][currentCol] == 9)
+                {
+                    break;
+                }
+                else
+                {
+                    basins[currentBasin].Add((currentRow, currentCol));
+                    CheckBasinAgain(matrix, currentBasin, currentRow, currentCol);
+                }
+                currentCol -= 1;
+            }
+            currentCol = col;
+
+            //check right
+            while (currentCol < matrix[0].Length)
+            {
+                if (matrix[currentRow][currentCol] == 9)
+                {
+                    break;
+                }
+                else
+                {
+                    basins[currentBasin].Add((currentRow, currentCol));
+                    CheckBasinAgain(matrix, currentBasin, currentRow, currentCol);
+                }
+                currentCol += 1;
+            }
+
+            return;
+        }
+
+        private static void CheckBasinAgain(List<int[]> matrix, int currentBasin, int row, int col)
+        {
+            var currentRow = row;
+            var currentCol = col;
+
+            //check up
+            while (currentRow >= 0)
+            {
+                if (matrix[currentRow][currentCol] == 9)
+                {
+                    break;
+                }
+                else
+                {
+                    basins[currentBasin].Add((currentRow, currentCol));
+                }
+                currentRow -= 1;
+            }
+            currentRow = row;
+
+            //check down
+            while (currentRow < matrix.Count)
+            {
+                if (matrix[currentRow][currentCol] == 9)
+                {
+                    break;
+                }
+                else
+                {
+                    basins[currentBasin].Add((currentRow, currentCol));
+                }
+                currentRow += 1;
+            }
+            currentRow = row;
+
+            //check left
+            while (currentCol >= 0)
+            {
+                if (matrix[currentRow][currentCol] == 9)
+                {
+                    break;
+                }
+                else
+                {
+                    basins[currentBasin].Add((currentRow, currentCol));
+                }
+                currentCol -= 1;
+            }
+            currentCol = col;
+
+            //check right
+            while (currentCol < matrix[0].Length)
+            {
+                if (matrix[currentRow][currentCol] == 9)
+                {
+                    break;
+                }
+                else
+                {
+                    basins[currentBasin].Add((currentRow, currentCol));
+                }
+                currentCol += 1;
+            }
+
+            return;
+        }
+
+        public static void Task01()
+        {
+            var matrix = new List<int[]>();
+
+            while (true)
+            {
+                var inputLine = Console.ReadLine();
+
+                if (inputLine == "end")
+                {
+                    break;
+                }
+
+                matrix.Add(new int[inputLine.Length]);
+                for (int i = 0; i < inputLine.Length; i++)
+                {
+                    matrix[matrix.Count - 1][i] = int.Parse(inputLine[i].ToString());
+                }
+            }
+
+            var riskLevel = 0;
+
+            for (int row = 0; row < matrix.Count; row++)
+            {
+                for (int col = 0; col < matrix[0].Length; col++)
+                {
+                    var element = matrix[row][col];
+                    
+                    var up = row != 0 ? matrix[row - 1][col] : int.MaxValue;
+                    var down = row != matrix.Count - 1 ? matrix[row + 1][col] : int.MaxValue;
+                    var left = col != 0 ? matrix[row][col - 1] : int.MaxValue;
+                    var right = col != matrix[0].Length - 1 ? matrix[row][col + 1] : int.MaxValue;
+
+                    if (element < up && element < down && element < left && element < right)
+                    {
+                        riskLevel += (element + 1);
+                    }                    
+                }
+            }
+
+            Console.WriteLine("Risk Level: " + riskLevel);
+        }
+    }
+}
---
+++ AdventOfCode2021/Day10/Program.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day10
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Task01();
+            Task02();
+        }
+
+        private static void Task02()
+        {
+            var parenthesis = new List<string>();
+
+            while (true)
+            {
+                var input = Console.ReadLine();
+
+                if (input == "end")
+                {
+                    break;
+                }
+
+                parenthesis.Add(input);
+            }
+
+            var points = new Dictionary<char, int>
+            {
+                { ')', 1 },
+                { ']', 2 },
+                { '}', 3 },
+                { '>', 4 },
+            };
+
+            var scores = new List<long>();
+
+            for (int line = 0; line < parenthesis.Count; line++)
+            {
+                var openingBrackets = new Stack<char>();
+                var isValid = true;
+
+                for (int i = 0; i < parenthesis[line].Length; i++)
+                {
+                    var bracket = parenthesis[line][i];
+
+                    if (bracket == '('
+                        || bracket == '['
+                        || bracket == '{'
+                        || bracket == '<')
+                    {
+                        openingBrackets.Push(bracket);
+                    }
+                    else
+                    {
+                        var lastOpeningBracket = openingBrackets.Pop();
+
+                        if ((bracket == ')' && lastOpeningBracket != '(')
+                            || (bracket == ']' && lastOpeningBracket != '[')
+                            || (bracket == '}' && lastOpeningBracket != '{')
+                            || (bracket == '>' && lastOpeningBracket != '<'))
+                        {
+                            isValid = false;
+                            break;
+                        }
+                        else
+                        {
+                            continue;
+                        }
+                    }
+                }
+                if (isValid && openingBrackets.Count > 0)
+                {
+                    scores.Add(0);
+                    var scoresIndex = scores.Count - 1;
+                    
+                    while (openingBrackets.Count > 0)
+                    {
+                        var openingBracket = openingBrackets.Pop();
+
+                        scores[scoresIndex] *= 5;
+
+                        switch (openingBracket)
+                        {
+                            case '(':
+                                scores[scoresIndex] += points[')'];
+                                break;
+
+                            case '[':
+                                scores[scoresIndex] += points[']'];
+                                break;
+
+                            case '{':
+                                scores[scoresIndex] += points['}'];
+                                break;
+
+                            case '<':
+                                scores[scoresIndex] += points['>'];
+                                break;
+
+                        }
+                    }
+                }
+            }
+
+            var score = scores
+                .OrderBy(x => x)
+                .Skip((scores.Count - 1) / 2)
+                .Take(1)
+                .ToList()[0];
+
+            Console.WriteLine("Score: " + score);
+        }
+
+        private static void Task01()
+        {
+            var parenthesis = new List<string>();
+
+            while (true)
+            {
+                var input = Console.ReadLine();
+
+                if (input == "end")
+                {
+                    break;
+                }
+
+                parenthesis.Add(input);
+            }
+
+            var points = new Dictionary<char, int>
+            {
+                { ')', 3 },
+                { ']', 57 },
+                { '}', 1197 },
+                { '>', 25137 },
+            };
+
+            var score = 0;
+
+            for (int line = 0; line < parenthesis.Count; line++)
+            {
+                var openingBrackets = new Stack<char>();
+                
+                for (int i = 0; i < parenthesis[line].Length; i++)
+                {
+                    var bracket = parenthesis[line][i];
+
+                    if (bracket == '('
+                        || bracket == '['
+                        || bracket == '{'
+                        || bracket == '<')
+                    {
+                        openingBrackets.Push(bracket);
+                    }
+                    else
+                    {
+                        var lastOpeningBracket = openingBrackets.Pop();
+
+                        if ((bracket == ')' && lastOpeningBracket != '(')
+                            || (bracket == ']' && lastOpeningBracket != '[')
+                            || (bracket == '}' && lastOpeningBracket != '{')
+                            || (bracket == '>' && lastOpeningBracket != '<'))
+                        {
+                            score += points[bracket];
+                            break;
+                        }
+                        else
+                        {
+                            continue;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine("Score: " + score);
+        }
+    }
+}
