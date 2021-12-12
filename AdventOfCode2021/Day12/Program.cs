374313caba77e55d1e7ffdf63764a484d389bc41
marinakolova <marina.kolova.359@gmail.com>
2021-12-12 13:36:06 +0200

    added day 12 solution

---
+++ AdventOfCode2021/Day12/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+
+namespace Day12
+{
+    public class Program
+    {
+        private static Dictionary<string, List<string>> graph;
+        
+        public static void Main(string[] args)
+        {
+            //Task01();
+            Task02();
+        }
+
+        private static void Task02()
+        {
+            graph = ReadInput();            
+
+            var result = FindPathsCount("start", ImmutableHashSet.Create<string>("start"), false, true);
+            Console.WriteLine("Paths: " + result);
+        }
+
+        private static void Task01()
+        {
+            graph = ReadInput();
+
+            var result = FindPathsCount("start", ImmutableHashSet.Create<string>("start"), false, false);
+            Console.WriteLine("Paths: " + result);
+        }
+
+        private static int FindPathsCount(string current, ImmutableHashSet<string> visited, bool isSmallVisitedTwice, bool canVisitSmallTwice)
+        {
+            if (current == "end")
+            {
+                return 1;
+            }
+
+            var pathsCount = 0;
+
+            foreach (var edge in graph[current])
+            {
+                var isBig = edge.ToUpper() == edge;
+                var seen = visited.Contains(edge);
+
+                if (!seen || isBig)
+                {                    
+                    pathsCount += FindPathsCount(edge, visited.Add(edge), isSmallVisitedTwice, canVisitSmallTwice);
+                }
+                else if (canVisitSmallTwice && !isBig && edge != "start" && !isSmallVisitedTwice)
+                {                    
+                    pathsCount += FindPathsCount(edge, visited, true, canVisitSmallTwice);
+                }
+            }
+
+            return pathsCount;
+        }
+
+        private static Dictionary<string, List<string>> ReadInput()
+        {
+            var graph = new Dictionary<string, List<string>>();
+            
+            while (true)
+            {
+                var input = Console.ReadLine();
+
+                if (input == "end")
+                {
+                    break;
+                }
+
+                var inputParts = input.Split("-");
+                var firstEdge = inputParts[0];
+                var secondEdge = inputParts[1];
+
+                if (!graph.ContainsKey(firstEdge))
+                {
+                    graph[firstEdge] = new List<string>();
+                }
+                if (!graph.ContainsKey(secondEdge))
+                {
+                    graph[secondEdge] = new List<string>();
+                }
+
+                graph[firstEdge].Add(secondEdge);
+                graph[secondEdge].Add(firstEdge);
+            }
+
+            return graph;
+        }
+    }
+}
