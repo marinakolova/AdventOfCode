971269d2293036f39fe64fc7a4952a0ae7d28152
marinakolova <marina.kolova.359@gmail.com>
2022-12-06 19:13:25 +0200

    added day 6 solution

--- AdventOfCode2022/AdventOfCode2022/Solutions/Day06.cs
+++ AdventOfCode2022/AdventOfCode2022/Solutions/Day06.cs
@@ -4,11 +4,33 @@
     {
         public static void Task01()
         {
-
+            FindMarker(4);
         }
+
         public static void Task02()
         {
+            FindMarker(14);
+        }
 
+        private static void FindMarker(int charsCount)
+        {
+            var signal = Console.ReadLine().ToString();
+
+            for (int i = charsCount; i < signal.Length; i++)
+            {
+                var uniqueChars = new HashSet<char>();
+
+                for (int j = 0; j < charsCount; j++)
+                {
+                    uniqueChars.Add(signal[i - j]);
+                }
+
+                if (uniqueChars.Count == charsCount)
+                {
+                    Console.WriteLine(i + 1);
+                    break;
+                }
+            }
         }
     }
 }
