1cb68ec48109ae196a24db88590de24bbb435dd8
marinakolova <marina.kolova.359@gmail.com>
2023-12-03 18:49:42 +0200

    added days 01-03 solutions

--- AdventOfCode2023/AdventOfCode2023/Day03.cs
+++ AdventOfCode2023/AdventOfCode2023/Day03.cs
@@ -4,12 +4,272 @@
     {
         public static void Task01()
         {
-            throw new NotImplementedException();
+            var schema = new List<List<char>>();
+            var partNumbersSum = 0;
+
+            while (true)
+            {
+                var line = Console.ReadLine();
+                if (line == "END")
+                {
+                    break;
+                }
+                var chars = new List<char>();
+                for (int i = 0; i < line.Length; i++)
+                {
+                    chars.Add(line[i]);
+                }
+                schema.Add(chars);
+            }
+
+            for (int row = 0; row < schema.Count; row++)
+            {
+                for (int col = 0; col < schema[row].Count; col++)
+                {
+                    if (char.IsDigit(schema[row][col]))
+                    {
+                        var digits = new List<char>();
+                        digits.Add(schema[row][col]);
+                        while (schema[row].Count - 1 >= col + 1 && char.IsDigit(schema[row][col + 1]))
+                        {
+                            digits.Add(schema[row][col + 1]);
+                            col += 1;
+                        }
+
+                        var number = int.Parse(string.Join("", digits));
+                        var surroundingChars = new List<char>();
+
+                        if (row > 0)
+                        {
+                            //check up & up left
+                            for (int i = 0; i <= digits.Count; i++)
+                            {
+                                if (col - i >= 0)
+                                {
+                                    surroundingChars.Add(schema[row - 1][col - i]);
+                                }
+                            }
+                            if (schema[row - 1].Count -1 >= col + 1)
+                            {
+                                // check up right
+                                surroundingChars.Add(schema[row - 1][col + 1]);
+                            }
+                        }
+                        if (col - digits.Count >= 0)
+                        {
+                            // check left
+                            surroundingChars.Add(schema[row][col - digits.Count]);
+                        }
+                        if (schema[row].Count - 1 >= col +1)
+                        {
+                            // check right
+                            surroundingChars.Add(schema[row][col + 1]);
+                        }
+                        if (schema.Count - 1 > row)
+                        {
+                            // check down & down left
+                            for (int i = 0; i <= digits.Count; i++)
+                            {
+                                if (col - i >= 0)
+                                {
+                                    surroundingChars.Add(schema[row + 1][col - i]);
+                                }
+                            }
+                            if (schema[row + 1].Count - 1 >= col + 1)
+                            {
+                                // check down right
+                                surroundingChars.Add(schema[row + 1][col + 1]);
+                            }
+                        }
+
+                        if (surroundingChars.Any(x => x != '.' && !char.IsDigit(x)))
+                        {
+                            partNumbersSum += number;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(partNumbersSum);
         }
 
         public static void Task02()
         {
-            throw new NotImplementedException();
+            var schema = new List<List<char>>();
+            var gears = new Dictionary<(int,int), List<int>>();
+
+            while (true)
+            {
+                var line = Console.ReadLine();
+                if (line == "END")
+                {
+                    break;
+                }
+                var chars = new List<char>();
+                for (int i = 0; i < line.Length; i++)
+                {
+                    chars.Add(line[i]);
+                }
+                schema.Add(chars);
+            }
+
+            for (int row = 0; row < schema.Count; row++)
+            {
+                for (int col = 0; col < schema[row].Count; col++)
+                {
+                    if (char.IsDigit(schema[row][col]))
+                    {
+                        var digits = new List<char>();
+                        digits.Add(schema[row][col]);
+                        while (schema[row].Count - 1 >= col + 1 && char.IsDigit(schema[row][col + 1]))
+                        {
+                            digits.Add(schema[row][col + 1]);
+                            col += 1;
+                        }
+
+                        var number = int.Parse(string.Join("", digits));
+
+                        if (row > 0)
+                        {
+                            //check up & up left
+                            for (int i = 0; i <= digits.Count; i++)
+                            {
+                                if (col - i >= 0)
+                                {
+                                    var checkedRow = row - 1;
+                                    var checkedCol = col - i;
+                                    var foundChar = schema[row - 1][col - i];
+
+                                    if (foundChar == '*')
+                                    {
+                                        if (gears.ContainsKey((checkedRow, checkedCol)))
+                                        {
+                                            gears[(checkedRow, checkedCol)].Add(number);
+                                        }
+                                        else
+                                        {
+                                            gears.Add((checkedRow, checkedCol), new List<int> { number });
+                                        }
+                                    }
+                                }
+                            }
+                            if (schema[row - 1].Count - 1 >= col + 1)
+                            {
+                                // check up right
+                                var checkedRow = row - 1;
+                                var checkedCol = col + 1;
+                                var foundChar = schema[row - 1][col + 1];
+
+                                if (foundChar == '*')
+                                {
+                                    if (gears.ContainsKey((checkedRow, checkedCol)))
+                                    {
+                                        gears[(checkedRow, checkedCol)].Add(number);
+                                    }
+                                    else
+                                    {
+                                        gears.Add((checkedRow, checkedCol), new List<int> { number });
+                                    }
+                                }
+                            }
+                        }
+                        if (col - digits.Count >= 0)
+                        {
+                            // check left
+                            var checkedRow = row;
+                            var checkedCol = col - digits.Count;
+                            var foundChar = schema[row][col - digits.Count];
+
+                            if (foundChar == '*')
+                            {
+                                if (gears.ContainsKey((checkedRow, checkedCol)))
+                                {
+                                    gears[(checkedRow, checkedCol)].Add(number);
+                                }
+                                else
+                                {
+                                    gears.Add((checkedRow, checkedCol), new List<int> { number });
+                                }
+                            }
+                        }
+                        if (schema[row].Count - 1 >= col + 1)
+                        {
+                            // check right
+                            var checkedRow = row;
+                            var checkedCol = col + 1;
+                            var foundChar = schema[row][col + 1];
+
+                            if (foundChar == '*')
+                            {
+                                if (gears.ContainsKey((checkedRow, checkedCol)))
+                                {
+                                    gears[(checkedRow, checkedCol)].Add(number);
+                                }
+                                else
+                                {
+                                    gears.Add((checkedRow, checkedCol), new List<int> { number });
+                                }
+                            }
+                        }
+                        if (schema.Count - 1 > row)
+                        {
+                            // check down & down left
+                            for (int i = 0; i <= digits.Count; i++)
+                            {
+                                if (col - i >= 0)
+                                {
+                                    var checkedRow = row + 1;
+                                    var checkedCol = col - i;
+                                    var foundChar = schema[row + 1][col - i];
+
+                                    if (foundChar == '*')
+                                    {
+                                        if (gears.ContainsKey((checkedRow, checkedCol)))
+                                        {
+                                            gears[(checkedRow, checkedCol)].Add(number);
+                                        }
+                                        else
+                                        {
+                                            gears.Add((checkedRow, checkedCol), new List<int> { number });
+                                        }
+                                    }
+                                }
+                            }
+                            if (schema[row + 1].Count - 1 >= col + 1)
+                            {
+                                // check down right
+                                var checkedRow = row + 1;
+                                var checkedCol = col + 1;
+                                var foundChar = schema[row + 1][col + 1];
+
+                                if (foundChar == '*')
+                                {
+                                    if (gears.ContainsKey((checkedRow, checkedCol)))
+                                    {
+                                        gears[(checkedRow, checkedCol)].Add(number);
+                                    }
+                                    else
+                                    {
+                                        gears.Add((checkedRow, checkedCol), new List<int> { number });
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            var gearRatiosSum = 0;
+            foreach (var gear in gears)
+            {
+                if (gear.Value.Count == 2)
+                {
+                    var gearRatio = gear.Value[0] * gear.Value[1];
+                    gearRatiosSum += gearRatio;
+                }
+            }
+
+            Console.WriteLine(gearRatiosSum);
         }
     }
 }
--- AdventOfCode2023/AdventOfCode2023/Program.cs
+++ AdventOfCode2023/AdventOfCode2023/Program.cs
@@ -4,14 +4,14 @@
     {
         public static void Main(string[] args)
         {
-            Day01.Task01();
-            Day01.Task02();
+            //Day01.Task01();
+            //Day01.Task02();
 
             //Day02.Task01();
             //Day02.Task02();
 
             //Day03.Task01();
-            //Day03.Task02();
+            Day03.Task02();
 
             //Day04.Task01();
             //Day04.Task02();
