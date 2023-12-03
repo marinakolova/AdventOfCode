5cd07d223699a87e33739bb0d233e7f495da043e
marinakolova <marina.kolova.359@gmail.com>
2023-12-03 14:45:20 +0200

    remove blank solutions

--- AdventOfCode2022/AdventOfCode2022/Program.cs
+++ AdventOfCode2022/AdventOfCode2022/Program.cs
@@ -44,42 +44,6 @@
 
             Day13.Task01();
             Day13.Task02();
-
-            //Day14.Task01();
-            //Day14.Task02();
-
-            //Day15.Task01();
-            //Day15.Task02();
-
-            //Day16.Task01();
-            //Day16.Task02();
-
-            //Day17.Task01();
-            //Day17.Task02();
-
-            //Day18.Task01();
-            //Day18.Task02();
-
-            //Day19.Task01();
-            //Day19.Task02();
-
-            //Day20.Task01();
-            //Day20.Task02();
-
-            //Day21.Task01();
-            //Day21.Task02();
-
-            //Day22.Task01();
-            //Day22.Task02();
-
-            //Day23.Task01();
-            //Day23.Task02();
-
-            //Day24.Task01();
-            //Day24.Task02();
-
-            //Day25.Task01();
-            //Day25.Task02();
         }
     }
 }
