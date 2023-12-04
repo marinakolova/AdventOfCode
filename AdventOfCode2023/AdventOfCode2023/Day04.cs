92498ff5014d921895e2b70cba635bbe32936929
marinakolova <marina.kolova.359@gmail.com>
2023-12-04 19:15:12 +0200

    added day 04 solution

--- AdventOfCode2023/AdventOfCode2023/Day04.cs
+++ AdventOfCode2023/AdventOfCode2023/Day04.cs
@@ -1,15 +1,106 @@
-﻿namespace AdventOfCode2023
+﻿using System.Numerics;
+
+namespace AdventOfCode2023
 {
     public static class Day04
     {
         public static void Task01()
         {
-            throw new NotImplementedException();
+            var points = 0;
+
+            while (true)
+            {
+                var line = Console.ReadLine();
+
+                if (line == "END")
+                {
+                    break;
+                }
+
+                var card = line.Trim().Split(": ", StringSplitOptions.RemoveEmptyEntries);
+                var numbers = card[1].Trim().Split(" | ", StringSplitOptions.RemoveEmptyEntries);
+                var winningNumbers = numbers[0].Trim().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+                var cardNumbers = numbers[1].Trim().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+                var cardPoints = 0;
+
+                foreach (var cardNumber in cardNumbers)
+                {
+                    if (winningNumbers.Any(x => x == cardNumber))
+                    {
+                        if (cardPoints == 0)
+                        {
+                            cardPoints += 1;
+                        }
+                        else
+                        {
+                            cardPoints *= 2;
+                        }
+                    }
+                }
+
+                points += cardPoints;
+            }
+
+            Console.WriteLine(points);
         }
 
         public static void Task02()
         {
-            throw new NotImplementedException();
+            var cards = new List<Card>();
+
+            while (true)
+            {
+                var line = Console.ReadLine();
+
+                if (line == "END")
+                {
+                    break;
+                }
+
+                var card = line.Trim().Split(": ", StringSplitOptions.RemoveEmptyEntries);
+                var cardId = int.Parse(card[0].Trim().Split(" ", StringSplitOptions.RemoveEmptyEntries)[1]);
+                var numbers = card[1].Trim().Split(" | ", StringSplitOptions.RemoveEmptyEntries);
+                var winningNumbers = numbers[0].Trim().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+                var cardNumbers = numbers[1].Trim().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+                var cardPoints = 0;
+
+                foreach (var cardNumber in cardNumbers)
+                {
+                    if (winningNumbers.Any(x => x == cardNumber))
+                    {
+                        cardPoints += 1;
+                    }
+                }
+
+                cards.Add(new Card
+                {
+                    Id = cardId,
+                    Points = cardPoints,
+                });
+            }
+
+            for (int i = 0; i < cards.Count; i++)
+            {
+                for (int j = 0; j < cards[i].Points; j++)
+                {
+                    var currentCardId = cards[i].Id;
+                    var currentCardIndex = currentCardId - 1;
+
+                    var cardToAdd = cards[currentCardIndex + 1 + j];
+                    cards.Add(cardToAdd);
+                }
+            }
+
+            Console.WriteLine(cards.Count);
         }
+    }
+
+    class Card
+    {
+        public int Id { get; set; }
+
+        public int Points { get; set; }
     }
 }
